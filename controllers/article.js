52e17a2d12c144f224ed02d51743e9156a7e61c9
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-01 17:28:36 +0100

    Did some work on functionality for creating an article and getting a published article

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,6 @@
 import { isValidObjectId } from "mongoose";
 import { Article, validateArticle } from "../models/article.js";
+import readingTime from "reading-time";
 
 export const getAllPublishedArticles = async (req, res) => {
   const publishedArticles = await Article.find({ state: "published" });
@@ -12,11 +13,14 @@ export const getAPublishedArticle = async (req, res) => {
     return res.status(400).json({ message: "This ID is not valid!" });
 
   const article = await Article.findById(id);
+  console.log(article);
   if (!article)
     return res.status(404).json({ message: "This article does not exist!" });
 
   if (article && article.state !== "published")
-    return res.status("This article has not yet been published!");
+    return res
+      .status(400)
+      .json({ message: "This article has not yet been published!" });
 
   return res.status(200).json(article);
 };
@@ -24,6 +28,22 @@ export const getAPublishedArticle = async (req, res) => {
 export const createAnArticle = async (req, res) => {
   // Validate the request body
   const { error } = validateArticle(req.body);
-  console.log(error);
-  res.send(error);
+  if (error) return res.status(400).json({ message: error.details[0].message });
+
+  // Check if title is taken
+  const title_taken = await Article.findOne({ title: req.body.title });
+  if (title_taken)
+    return res.status(400).json({ message: "This title is already taken!" });
+  // Get the reading time of the body
+  const reading_time = readingTime(req.body.body);
+
+  // Create the article
+  const article = new Article({
+    ...req.body,
+    reading_time: reading_time.text,
+  });
+
+  const result = await article.save();
+
+  return res.send(result);
 };
