530ff454658c27a4ba834ebacd9de6b354b6c168
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-01 17:27:58 +0100

    Added morgan for logging

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import connectToDb from "./startup/db.js";
 import dotenv from "dotenv";
 import useRoutesFor from "./startup/routes.js";
 import cors from "cors";
+import logger from "morgan";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ dotenv.config();
 // Middleware functions
 app.use(express.json());
 app.use(cors());
+app.use(logger("dev"));
 useRoutesFor(app);
 app.listen(port, () =>
   console.log(`The server is now running on port ${port}...`)
