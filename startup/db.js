ef9f007f9393c8b30a65c018fcfbc6b543e83eaa
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-05 14:10:33 +0100

    removed dotenv and started using config

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import config from "config"
 
 export default function connectToDB() {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(config.get("MONGO_URI"))
     .then(() => console.log("Connected to the DB..."))
     .catch((err) => {
       console.error(err.message);
