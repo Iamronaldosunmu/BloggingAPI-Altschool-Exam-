1ed9c9bbf8156c49dfe8db02e8814143948048d4
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-05 14:12:14 +0100

    Added the article routes

--- routes/article.js
+++ routes/article.js
@@ -1,13 +1,48 @@
 import express from "express";
 import {
   createAnArticle,
+  deleteAnArticle,
+  editArticle,
   getAllPublishedArticles,
   getAPublishedArticle,
+  getArticlesByAuthor,
+  publishAnArticle,
 } from "../controllers/article.js";
+import passport from "passport";
 const articleRouter = express.Router();
 
 articleRouter.get("/", getAllPublishedArticles);
-articleRouter.post("/", createAnArticle);
-articleRouter.get("/:id", getAPublishedArticle)
+
+articleRouter.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  createAnArticle
+);
+
+articleRouter.get("/:id", getAPublishedArticle);
+
+articleRouter.post(
+  "/:id/publish",
+  passport.authenticate("jwt", { session: false }),
+  publishAnArticle
+);
+
+articleRouter.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  editArticle
+);
+
+articleRouter.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  deleteAnArticle
+);
+
+articleRouter.get(
+  "/authors/my_articles",
+  passport.authenticate("jwt", { session: false }),
+  getArticlesByAuthor
+);
 
 export default articleRouter;
