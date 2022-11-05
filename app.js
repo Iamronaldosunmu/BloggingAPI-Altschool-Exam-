4c4d3bec3418ba5f45ccae2139ed36f69fda1390
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-05 14:17:21 +0100

    Removed dotenv and used config instead

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectToDb from "./startup/db.js";
-import dotenv from "dotenv";
+import config from "config";
 import useRoutesFor from "./startup/routes.js";
 import cors from "cors";
 import logger from "morgan";
@@ -8,7 +8,7 @@ import logger from "morgan";
 const app = express();
 const port = process.env.PORT || 4000;
 
-dotenv.config();
+import "./controllers/user.js";
 // Middleware functions
 app.use(express.json());
 app.use(cors());
@@ -18,3 +18,5 @@ app.listen(port, () =>
   console.log(`The server is now running on port ${port}...`)
 );
 connectToDb();
+
+export default app;
