5f5e7a4f4b92761e934aeabbc6d3a7130210c36f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-11-05 14:12:36 +0100

    Added methods to validate user inputs

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
+import Joi from "joi";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
   first_name: {
     type: String,
     required: true,
@@ -14,6 +16,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     min: 3,
   },
   password: {
@@ -22,5 +25,32 @@ const userSchema = new mongoose.Schema({
     min: 3,
   },
 });
+
+userSchema.methods.isValidPassword = async (password_supplied, user_password) => {
+  const isValid = await bcrypt.compare(password_supplied, user_password);
+  return isValid;
+};
+
+export const validateUser = (user) => {
+  const schema = Joi.object({
+    first_name: Joi.string().min(3).max(30).required(),
+    last_name: Joi.string().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: false } })
+      .required(),
+    password: Joi.string().min(8).max(80).required(),
+  });
+  return schema.validate(user);
+};
+
+export const validateUserLogin = (user) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: false } })
+      .required(),
+    password: Joi.string().min(8).max(80).required(),
+  });
+  return schema.validate(user);
+};
 
 export const User = mongoose.model("User", userSchema);
